46bd1e2414e322ac4c6e5d444d4f0ed09b8bb7f9
PonyCui <cuiminghui@yy.com>
2015-11-24 10:00:46 +0800

    Initial Commit

---
+++ AsyncDisplayKit-Issue-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AsyncDisplayKit-Issue-2
+//
+//  Created by 崔 明辉 on 15/11/24.
+//  Copyright © 2015年 AsyncDisplayKit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
