dc1067ba0dc40d2026fb840811b1d65511ee06f9
PonyCui <cuiminghui@yy.com>
2015-11-24 10:56:14 +0800

    Commit

--- AsyncDisplayKit-Issue-2/ViewController.swift
+++ AsyncDisplayKit-Issue-2/ViewController.swift
@@ -12,14 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureLabel()
+        configureImageView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func configureLabel() {
+        let textLabel = ASTextNode()
+        textLabel.attributedString = NSAttributedString(string: "Hello, World!",
+            attributes: [
+                NSForegroundColorAttributeName: UIColor.blackColor(),
+                NSFontAttributeName: UIFont.systemFontOfSize(17)
+            ])
+        textLabel.measure(CGSize(width: view.bounds.width, height: CGFloat.max))
+        textLabel.frame = CGRect(x: 0, y: 50, width: textLabel.calculatedSize.width, height: textLabel.calculatedSize.height)
+        self.view.addSubnode(textLabel)
     }
-
+    
+    func configureImageView() {
+        let imageView = ASNetworkImageNode(cache: ImageManager.sharedManager,
+            downloader: ImageManager.sharedManager)
+        imageView.frame = CGRect(x: 0, y: 100, width: view.bounds.size.width, height: 300)
+        imageView.contentMode = .ScaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.URL = NSURL(string: "http://ww3.sinaimg.cn/large/7191b2ebgw1eyb1uontv4j21w02iou0r.jpg")!
+        self.view.addSubnode(imageView)
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '7.0'
+
+target 'AsyncDisplayKit-Issue-2' do
+
+pod "AsyncDisplayKit"
+pod "SDWebImage"
+
+end
+
